cdfbe50cb359ae4c984b7684622370f573c60f63
AkimovIgor <akimovigor5417@gmail.com>
2020-06-14 13:26:14 +0300

    16 lesson

--- src/components/MyPosts/MyPosts.js
+++ src/components/MyPosts/MyPosts.js
@@ -4,9 +4,24 @@ import Post from "./Post/Post";
 const MyPosts = () => {
   return (
     <div>
-        <Post/>
-        <Post/>
-        <Post/>
+        <Post
+          title='Sample blog post 1'
+          date='January 1, 2014'
+          autor='Mark'
+          text='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        />
+        <Post
+          title='Sample blog post 2'
+          date='January 6, 2017'
+          autor='Mark'
+          text='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        />
+        <Post
+          title='Sample blog post 3'
+          date='January 23, 2020'
+          autor='Mark'
+          text='Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
+        />
     </div>
   )
 }
